45cef56a486d923cc4b6eadc7803a862e912ecbb
isyatiradhiyah <162080680+isyatiradhiyah@users.noreply.github.com>
2024-03-12 14:34:05 +0700

    Add files via upload

---
+++ diyah.py
@@ -0,0 +1,9 @@
+print("MENGHITUNG LUAS DAN KELILING TRAPESIUM SAMA KAKI")
+a = float(input("masukkan sisi atas trapesium sama kaki = "))
+b = float(input("masukkan sisi bawah trapesium sama kaki = "))
+c = float(input("masukkan tinggi trapesium sama kaki = "))
+d = float(input("masukkan sisi samping trapesium sama kaki = "))
+luas = (a+b)*c*1/2
+keliling = a+b+2*d
+print("luas trapesium sama kaki adalah ", luas)
+print("keliling trapesium sama kaki adalah ", keliling)
