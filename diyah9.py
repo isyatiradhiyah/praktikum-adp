9132be41ac737f87363299754fead266d1340202
isyatiradhiyah <162080680+isyatiradhiyah@users.noreply.github.com>
2024-05-29 01:16:02 +0700

    Add files via upload

---
+++ diyah9.py
@@ -0,0 +1,24 @@
+from termcolor import cprint
+import os
+os.system('cls')
+for i in range (4) :
+    cprint(" "*3, "black", "on_red", end="  ")
+cprint(" "*8, "black", "on_red")
+for i in range (3) :
+    cprint(" "*3, "black", "on_red", end="  ")
+print("")
+for i in range (3) :
+    cprint(" "*3, "black", "on_red", end="  ")  
+cprint(" "*3, "black", "on_dark_grey", end="  ")
+cprint(" "*3, "black", "on_red")
+cprint(" "*3, "black", "on_red", end="  ")
+print(end="     ")
+cprint(" "*3, "black", "on_red", end="  ")
+cprint(" "*3, "black", "on_dark_grey", end="  ")
+cprint(" "*3, "black", "on_red")
+for i in range (2) :
+    cprint(" "*3, "black", "on_red", end="  ")
+    print(end="     ")
+    cprint(" "*3, "black", "on_red", end="  ")
+    cprint(" "*3, "black", "on_dark_grey", end="  ")
+    cprint(" "*3, "black", "on_red")
